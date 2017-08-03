6e70fdc8dfdce99dceeec6fd1d3b9aa7f5448da6
Philip Eriksson <Lominean@users.noreply.github.com>
2017-08-03 16:59:20 +0200

    Add some random code

--- src/callsign.js
+++ src/callsign.js
@@ -3,9 +3,38 @@
  * @see {@link https://github.com/Lominean/callsign.js|Repository at GitHub}
  */
 
-function csParser() {
+var prefixTable = {
+  "SA–SM": "SE",
+  "DA–DR": "DE",
+  "EM–EO": "UA"
+};
+
+function flag(code) {
   "use strict";
-
-  const csregex = /\D{1,3}\d\D{1,3}/;
-
+  return String.fromCodePoint(...[...code].map(c => c.charCodeAt() + 127397))
 }
+
+function examine(text) {
+  "use strict";
+  alert(text);
+}
+
+function traverse(ele) {
+  "use strict";
+  if (ele.childNodes.length == 0) {
+    examine(ele.textContent);
+  } else {
+    document.ele.childNodes.forEach(function () {
+      traverse(children);
+    });
+  }
+}
+
+function callsign() {
+  "use strict";
+  var csregex = /\D{1,3}\d\D{1,3}/;
+
+  traverse(document.body);
+}
+
+callsign();

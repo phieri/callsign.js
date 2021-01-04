d48a6bb29f9544d4307f7ffaba8e3b680eab02f5
Philip Eriksson <foo@philiperiksson.se>
2021-01-04 13:24:46 +0100

    Use Map instead of Object

--- src/callsign.js
+++ src/callsign.js
@@ -46,73 +46,73 @@ const PHONETIC_ALPHABET = new Map([
 ]);
 
 /** @constant */
-const PREFIX_TABLE = {
-  AF: ['YA', 'T6'],
-  AL: ['ZA'],
-  AR: ['AY', 'AZ', 'LO', 'LP', 'LQ', 'LR', 'LS', 'LT', 'LU', 'LV', 'LW'],
-  AT: ['OE'],
-  AU: ['AX', 'VH', 'VI', 'VJ', 'VK', 'VN', 'VZ'],
-  BG: ['LZ'],
-  BR: ['PP', 'PQ', 'PR', 'PS', 'PT', 'PU', 'PV', 'PW', 'PX', 'PY', 'ZV', 'ZW', 'ZX', 'ZY', 'ZZ'],
-  CA: ['CF', 'CG', 'CH', 'CI', 'CJ', 'CK', 'CY', 'CZ', 'VA', 'VB', 'VC', 'VD', 'VE', 'VF', 'VG', 'VO', 'VX', 'VY', 'XJ', 'XK', 'XL', 'XM', 'XN', 'XO'],
-  CF: ['TL'],
-  CH: ['HB', 'HE'],
-  CI: ['TU'],
-  CL: ['CA', 'CB', 'CC', 'CD', 'CE', 'XQ', 'XR', '3G'],
-  CM: ['TJ'],
-  CN: ['B', 'VR', 'XS', 'XX'],
-  CR: ['TE', 'TI'],
-  DE: ['DA', 'DB', 'DC', 'DD', 'DE', 'DF', 'DG', 'DH', 'DI', 'DJ', 'DK', 'DL', 'DM', 'DN', 'DO', 'DP', 'DQ', 'DR'],
-  DK: ['OU', 'OV', 'OW', 'OX', 'OY', 'OZ', 'XP'],
-  EE: ['ES'],
-  ES: ['AM', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'EA', 'EB', 'EC', 'ED', 'EE', 'EF', 'EG', 'EH'],
-  FI: ['OF', 'OG', 'OH', 'OI', 'OJ'],
-  FR: ['F', 'HW', 'HX', 'HY'],
-  GA: ['TR'],
-  GB: ['G', 'M', 'VP', 'VQ', 'VS', 'ZB-ZJ', 'ZN', 'ZO', 'ZQ'],
-  GT: ['TD', 'TG'],
-  GQ: ['3C'],
-  HN: ['HQ', 'HR'],
-  HT: ['4V', 'HH'],
-  HU: ['HA', 'HG'],
-  ID: ['AT', 'AU', 'AV', 'AW', 'VT', 'VU', 'VV', 'VW'],
-  IL: ['4X', '4Z'],
-  IQ: ['HN', 'YI'],
-  IS: ['TF'],
-  IT: ['I'],
-  JP: ['JA', 'JB', 'JC', 'JD', 'JE', 'JF', 'JG', 'JH', 'JI', 'JJ', 'JK', 'JL', 'JM', 'JN', 'JO', 'JP', 'JQ', 'JR', 'JS'],
-  KP: ['HM', 'P5', 'P6', 'P7', 'P8', 'P9'],
-  KR: ['DS', 'DT', 'HL'],
-  LT: ['LY'],
-  LV: ['YL'],
-  MC: ['3A'],
-  ML: ['TZ'],
-  MM: ['XY', 'XZ'],
-  MU: ['3B'],
-  NI: ['H6', 'H7', 'HT'],
-  NL: ['PA', 'PB', 'PC', 'PD', 'PE', 'PF', 'PG', 'PH', 'PI', 'PJ'],
-  NO: ['L', 'LB', 'LC', 'LD', 'LE', 'LF', 'LG', 'LH', 'LI', 'LJ', 'LK', 'LL', 'LM', 'LN'],
-  NZ: ['ZK', 'ZL', 'ZM'],
-  PK: ['AP', 'AQ', 'AR', 'AS', '6P', '6Q', '6R', '6S'],
-  PL: ['HF', 'SN', 'SO', 'SP', 'SQ', 'SR', '3Z'],
-  PY: ['ZP'],
-  RO: ['YO', 'YP', 'YQ', 'YR'],
-  RS: ['YT', 'YU'],
-  RU: ['R', 'UA', 'UB', 'UC', 'UD', 'UE', 'UF', 'UG', 'UH', 'UI'],
-  SE: ['SA', 'SB', 'SC', 'SD', 'SE', 'SF', 'SG', 'SH', 'SI', 'SJ', 'SK', 'SL', 'SM', '7S'],
-  SK: ['OM'],
-  SV: ['HU', 'YS'],
-  TD: ['TT'],
-  TH: ['HS'],
-  TR: ['TA', 'TB', 'TC', 'YM'],
-  UA: ['EM', 'EN', 'EO', 'UR', 'US', 'UT', 'UU', 'UV', 'UW', 'UX', 'UY', 'UZ'],
-  US: ['AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'K', 'N', 'W'],
-  VA: ['HV'],
-  VE: ['4M', 'YV', 'YW', 'YX', 'YY'],
-  VU: ['YJ'],
-  YE: ['7O'],
-  ZA: ['ZR', 'ZS', 'ZT', 'ZU'],
-};
+const PREFIX_TABLE = new Map([
+  ['AF', ['YA', 'T6']],
+  ['AL', ['ZA']],
+  ['AR', ['AY', 'AZ', 'LO', 'LP', 'LQ', 'LR', 'LS', 'LT', 'LU', 'LV', 'LW']],
+  ['AT', ['OE']],
+  ['AU', ['AX', 'VH', 'VI', 'VJ', 'VK', 'VN', 'VZ']],
+  ['BG', ['LZ']],
+  ['BR', ['PP', 'PQ', 'PR', 'PS', 'PT', 'PU', 'PV', 'PW', 'PX', 'PY', 'ZV', 'ZW', 'ZX', 'ZY', 'ZZ']],
+  ['CA', ['CF', 'CG', 'CH', 'CI', 'CJ', 'CK', 'CY', 'CZ', 'VA', 'VB', 'VC', 'VD', 'VE', 'VF', 'VG', 'VO', 'VX', 'VY', 'XJ', 'XK', 'XL', 'XM', 'XN', 'XO']],
+  ['CF', ['TL']],
+  ['CH', ['HB', 'HE']],
+  ['CI', ['TU']],
+  ['CL', ['CA', 'CB', 'CC', 'CD', 'CE', 'XQ', 'XR', '3G']],
+  ['CM', ['TJ']],
+  ['CN', ['B', 'VR', 'XS', 'XX']],
+  ['CR', ['TE', 'TI']],
+  ['DE', ['DA', 'DB', 'DC', 'DD', 'DE', 'DF', 'DG', 'DH', 'DI', 'DJ', 'DK', 'DL', 'DM', 'DN', 'DO', 'DP', 'DQ', 'DR']],
+  ['DK', ['OU', 'OV', 'OW', 'OX', 'OY', 'OZ', 'XP']],
+  ['EE', ['ES']],
+  ['ES', ['AM', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'EA', 'EB', 'EC', 'ED', 'EE', 'EF', 'EG', 'EH']],
+  ['FI', ['OF', 'OG', 'OH', 'OI', 'OJ']],
+  ['FR', ['F', 'HW', 'HX', 'HY']],
+  ['GA', ['TR']],
+  ['GB', ['G', 'M', 'VP', 'VQ', 'VS', 'ZB-ZJ', 'ZN', 'ZO', 'ZQ']],
+  ['GT', ['TD', 'TG']],
+  ['GQ', ['3C']],
+  ['HN', ['HQ', 'HR']],
+  ['HT', ['4V', 'HH']],
+  ['HU', ['HA', 'HG']],
+  ['ID', ['AT', 'AU', 'AV', 'AW', 'VT', 'VU', 'VV', 'VW']],
+  ['IL', ['4X', '4Z']],
+  ['IQ', ['HN', 'YI']],
+  ['IS', ['TF']],
+  ['IT', ['I']],
+  ['JP', ['JA', 'JB', 'JC', 'JD', 'JE', 'JF', 'JG', 'JH', 'JI', 'JJ', 'JK', 'JL', 'JM', 'JN', 'JO', 'JP', 'JQ', 'JR', 'JS']],
+  ['KP', ['HM', 'P5', 'P6', 'P7', 'P8', 'P9']],
+  ['KR', ['DS', 'DT', 'HL']],
+  ['LT', ['LY']],
+  ['LV', ['YL']],
+  ['MC', ['3A']],
+  ['ML', ['TZ']],
+  ['MM', ['XY', 'XZ']],
+  ['MU', ['3B']],
+  ['NI', ['H6', 'H7', 'HT']],
+  ['NL', ['PA', 'PB', 'PC', 'PD', 'PE', 'PF', 'PG', 'PH', 'PI', 'PJ']],
+  ['NO', ['L', 'LB', 'LC', 'LD', 'LE', 'LF', 'LG', 'LH', 'LI', 'LJ', 'LK', 'LL', 'LM', 'LN']],
+  ['NZ', ['ZK', 'ZL', 'ZM']],
+  ['PK', ['AP', 'AQ', 'AR', 'AS', '6P', '6Q', '6R', '6S']],
+  ['PL', ['HF', 'SN', 'SO', 'SP', 'SQ', 'SR', '3Z']],
+  ['PY', ['ZP']],
+  ['RO', ['YO', 'YP', 'YQ', 'YR']],
+  ['RS', ['YT', 'YU']],
+  ['RU', ['R', 'UA', 'UB', 'UC', 'UD', 'UE', 'UF', 'UG', 'UH', 'UI']],
+  ['SE', ['SA', 'SB', 'SC', 'SD', 'SE', 'SF', 'SG', 'SH', 'SI', 'SJ', 'SK', 'SL', 'SM', '7S']],
+  ['SK', ['OM']],
+  ['SV', ['HU', 'YS']],
+  ['TD', ['TT']],
+  ['TH', ['HS']],
+  ['TR', ['TA', 'TB', 'TC', 'YM']],
+  ['UA', ['EM', 'EN', 'EO', 'UR', 'US', 'UT', 'UU', 'UV', 'UW', 'UX', 'UY', 'UZ']],
+  ['US', ['AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'K', 'N', 'W']],
+  ['VA', ['HV']],
+  ['VE', ['4M', 'YV', 'YW', 'YX', 'YY']],
+  ['VU', ['YJ']],
+  ['YE', ['7O']],
+  ['ZA', ['ZR', 'ZS', 'ZT', 'ZU']],
+]);
 
 /** @constant */
 const SEARCH_REGEX = /([A-Z,\d]{1,3}\d[A-Z]{1,3}(?:\/\d)?)\s/;
@@ -135,10 +135,11 @@ class Callsign extends HTMLElement {
     }
 
     const match = this.innerHTML.match(PARTS_REGEX);
-    const found = new Map();
-    found.set('prefix', match[1]);
-    found.set('digit', match[2]);
-    found.set('suffix', match[3]);
+    const found = new Map([
+      ['prefix', match[1]],
+      ['digit', match[2]],
+      ['suffix', match[3]],
+    ]);
 
     if (document.getElementById('callsign-js').dataset.phonetic != 'false') {
       const phonetic = Callsign.getPhonetics(match[0]);
@@ -149,7 +150,7 @@ class Callsign extends HTMLElement {
     if (document.getElementById('callsign-js').dataset.flag != 'false') {
       const flagElement = document.createElement('span');
 
-      for (let [iso, prefix] of Object.entries(PREFIX_TABLE)) {
+      for (let [iso, prefix] of PREFIX_TABLE) {
         if (prefix.includes(found.get('prefix'))) {
           flagElement.className = 'cs-flag';
           flagElement.title = iso;

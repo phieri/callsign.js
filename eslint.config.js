7a9855079a1eeec086ed522dfd803ca1df48394a
Philip Eriksson <12006381+phieri@users.noreply.github.com>
2024-06-04 23:59:55 +0200

    Upgrade eslint to v9

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    "rules": {
+      "indent": ["error", 2],
+      "complexity": ["error"],
+      "no-floating-decimal": ["error"],
+      "no-invalid-regexp": ["error"]
+    }
+  }
+];
